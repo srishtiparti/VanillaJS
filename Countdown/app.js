ba894102df9e4b1884bb4e330e5af62d116b73c6
Srishti Parti <srishtiparti97@gmail.com>
2022-03-31 21:31:54 -0400

    commit on main

--- Countdown/app.js
+++ Countdown/app.js
@@ -1,18 +1,49 @@
-const weekday = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
-const deadline = document.querySelector(".deadline");
+// declaring array of weekdays
 
-var curDate = new Date();
-var today = new Date();
-today.setDate(today.getDate() + 10);
-var furDate = weekday[today.getDay()] + ', ' + today.getDate() + ' ' + today.toLocaleString('default', { month: 'long' }) + ' ' + today.getFullYear()
+const weekday = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+
+// getting all constant values
+
+const deadline = document.querySelector(".deadline");
+const days = document.querySelector(".days");
+const hours = document.querySelector(".hours");
+const minutes = document.querySelector(".mins");
+const seconds = document.querySelector(".seconds");
+
+// setting future date 
+var futureDate = new Date();
+futureDate.setDate(futureDate.getDate() + 10);
+console.log(futureDate)
+var furYear = futureDate.getFullYear()
+var furDate = futureDate.getDate()
+var furMonth = futureDate.getMonth()
+
+var giveaway = weekday[futureDate.getDay()] + ', ' + furDate + ' ' + futureDate.toLocaleString('default', { month: 'long' }) + ' ' + furYear
 console.log(furDate)
 
-deadline.innerHTML = furDate;
+let date = new Date(furYear, furMonth, furDate, 11, 30, 0).getTime()
 
-// const _MS_PER_DAY = 24 * 60 * 60 * 1000
+// printing the giveaway date
+deadline.innerHTML = giveaway;
 
-// var utc1 = Date.UTC(today.getFullYear(), today.getMonth(), today.getDate());
-// var utc2 = Date.UTC(curDate.getFullYear(), curDate.getMonth(), curDate.getDate());
 
-// var days = (utc1 - utc2) / _MS_PER_DAY;
-// console.log(days)
+// setting intervals to 1 second
+setInterval(function() {
+    var curDate = new Date().getTime();
+
+    // calculating difference between now and future date (in milliseconds)
+    var difference = date - curDate
+
+    // calculating days, hours, minutes and secomds left
+    var daysleft = difference / (24 * 60 * 60 * 1000)
+    var hoursleft = (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    var minutesleft = (difference % (1000 * 60 * 60)) / (1000 * 60)
+    var secondsleft = (difference % (1000 * 60)) / (1000)
+
+    // printing days, hours, minutes and secomds left
+    days.innerHTML = Math.floor(daysleft)
+    hours.innerHTML = Math.floor(hoursleft)
+    minutes.innerHTML = Math.floor(minutesleft)
+    seconds.innerHTML = Math.floor(secondsleft)
+
+})
